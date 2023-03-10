1cfb2e8c772099acafffefe47c25ca1c8422fd22
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2023-03-10 11:23:57 +0100

    XC-8 Match selectors

--- assets/js/theme/global/modal.js
+++ assets/js/theme/global/modal.js
@@ -3,9 +3,9 @@ import trigger from '../common/utils/trigger';
 import q$, { q$$ } from './selector';
 
 const bodyActiveClass = 'has-active-modal';
-const loadingOverlayClass = 'js-loading-overlay';
+const loadingOverlayClass = 'is-loading-overlay';
 const modalBodyClass = 'js-modal-body';
-const modalContentClass = 'js-modal-content';
+const modalContentClass = 'is-modal-content';
 const modalCloseClass = 'js-modal-close';
 
 const SizeClasses = {
