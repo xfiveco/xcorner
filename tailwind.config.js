38e5f4367452ed0c80d38ed887c66aa34df26795
Hector Hurtado <hurtado.fernando@gmail.com>
2023-08-27 19:27:36 -0500

    adds max width content for desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./templates/**/*.html'],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                desktop: 'var(--max-w-desktop, 70rem)',
+            },
+        },
     },
     // eslint-disable-next-line import/no-extraneous-dependencies
     plugins: [require('@tailwindcss/forms')],
