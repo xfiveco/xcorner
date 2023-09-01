1fa810cab809b06c56f4ccf2da3c90ab651fa7c0
Aleksandra Worhacz <aleksandra.worhacz@xfive.co>
2023-09-01 16:37:26 +0200

    Style cart page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./templates/**/*.html'],
@@ -9,8 +12,29 @@ module.exports = {
             gridTemplateColumns: {
                 product: '48% 1fr',
             },
+            screens: {
+                xs: '460px',
+            },
         },
     },
     // eslint-disable-next-line import/no-extraneous-dependencies
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                '.x-hidden': {
+                    border: 0,
+                    clip: 'rect(0 0 0 0)',
+                    clipPath: 'insect(50%)',
+                    height: '1px',
+                    margin: '-1px',
+                    overflow: 'hidden',
+                    padding: 0,
+                    position: 'absolute',
+                    'white-space': 'nowrap',
+                    width: '1px',
+                },
+            })
+        }),
+    ],
 }
