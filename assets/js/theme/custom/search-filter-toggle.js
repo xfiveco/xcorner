dc23174abd91308bbc8422e1d407c0916484cfee
martibelegu <marti.belegu@xfive.co>
2024-02-29 19:19:04 +0100

    CR feedback

--- assets/js/theme/custom/search-filter-toggle.js
+++ assets/js/theme/custom/search-filter-toggle.js
@@ -1,13 +1,12 @@
 /**
  * Add hide search facets toggle in search page
- *
  */
 export default function searchFilterToggle() {
     const filterButton = document.querySelector('.js-search-hide-filters')
     const filterWrapper = document.querySelector('.js-search-facets')
     const filterButtonLabels = document.querySelectorAll('.js-hide-filters-label')
-    const filterButtonSVG = filterButton.querySelector('svg')
     if (filterButton) {
+        const filterButtonSVG = filterButton.querySelector('svg')
         filterButton.addEventListener('click', () => {
             filterWrapper.classList.toggle('is-open')
             filterButtonSVG.classList.toggle('is-open')
