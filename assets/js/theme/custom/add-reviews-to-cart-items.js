5606f50ba67b091ebe5006b3e764754649433844
martibelegu <marti.belegu@xfive.co>
2024-03-12 10:30:18 +0100

    CR feedback

--- assets/js/theme/custom/add-reviews-to-cart-items.js
+++ assets/js/theme/custom/add-reviews-to-cart-items.js
@@ -37,23 +37,17 @@ export default function addReviewsToCartItems(storefrontApiToken) {
 
                     ratingLink.innerHTML = `${numberOfReviews} reviews`
 
-                    const filledStarSVG = `
-                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
-                            <path d="M6 0L7.66869 4.21848L12 4.58359L8.7 7.55587L9.7082 12L6 9.61848L2.2918 12L3.3 7.55587L0 4.58359L4.33131 4.21848L6 0Z" fill="#C1A45A"></path>
-                        </svg>`
-
-                    const unfilledStarSVG = `
-                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
-                            <path d="M6 1.35931L7.20375 4.4024L7.31778 4.69068L7.62669 4.71672L10.8074 4.98484L8.36538 7.18435L8.14752 7.38056L8.21239 7.66649L8.95026 10.919L6.27019 9.19777L6 9.02425L5.72981 9.19777L3.04974 10.919L3.78761 7.66649L3.85247 7.38056L3.63462 7.18435L1.19259 4.98484L4.37331 4.71672L4.68222 4.69068L4.79625 4.4024L6 1.35931Z" stroke="#C1A45A"></path>
-                        </svg>`
-
                     if (ratingElement) {
-                        for (let i = 1; i <= 5; i++) {
-                            if (i <= summationOfRatings) {
-                                ratingElement.insertAdjacentHTML('beforeend', filledStarSVG)
-                            } else {
-                                ratingElement.insertAdjacentHTML('beforeend', unfilledStarSVG)
-                            }
+                        ratingElement.setAttribute('data-product-rating', summationOfRatings)
+                        const filledStars = ratingElement.querySelectorAll('.c-cart-item__filled-stars svg')
+                        const unfilledStars = ratingElement.querySelectorAll('.c-cart-item__unfilled-stars svg')
+
+                        for (let i = 0; i < summationOfRatings; i++) {
+                            filledStars[i].classList.remove('u-hidden')
+                        }
+
+                        for (let i = 0; i < 5 - summationOfRatings; i++) {
+                            unfilledStars[i].classList.remove('u-hidden')
                         }
                     }
                 })
