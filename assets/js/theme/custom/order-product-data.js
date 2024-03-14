d3f5b8c890418c1d8b13bb2b4edaec8855c7c1dc
Aleksandra Zając <aleksandra.worhacz@xfive.co>
2024-03-14 14:27:53 +0100

    XCORNER 26 Account Pages (#116)
    * Ignore .env file
    
    * Update icons
    
    * Add icon object
    
    * Add box object
    
    * Add grid object
    
    * Icon object's minot tweaks
    
    * Style login page + add few common objects
    
    * Minor tweaks
    
    * Use container object throughout the site. Update widget templates
    
    * Remove old o-container code
    
    * Add account layout
    
    * Add list obejct
    
    * Import missing container, text and heading objects' style
    
    * Style orders list
    
    * Style order's detail page + handle missing data
    
    * Style messages
    
    * Style addresses
    
    * Style wishlist
    
    * Update recently viiewed products layout & grid
    
    * Style account settings
    
    * Update products/product-view with container object
    
    * Update locales
    
    * Small tweaks - remove duplicated title -> heading object, use grid obejct to maintin spaces, remove not needed styles
    
    * Style info box
    
    * Wishlist list tweaks - fix displaying multiple lists, hide when no lists
    
    * Replace px with rem-calc function
    
    * Remove API KEY comments - apparently that's not an ugly way :D
    
    * Rename query

---
+++ assets/js/theme/custom/order-product-data.js
@@ -0,0 +1,54 @@
+export default function getOrderProductData(apiToken) {
+    const selectors = {
+        item: '.js-card-order',
+        url: '.js-dynamic-url',
+        wishlist: '.js-wishlist-add',
+    }
+
+    const items = Array.from(document.querySelectorAll(selectors.item))
+    items.forEach((item) => {
+        const links = Array.from(item.querySelectorAll(selectors.url))
+        const sku = item.getAttribute('data-sku')
+        const wishlistForm = item.querySelector(selectors.wishlist)
+
+        fetch('/graphql', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${apiToken}`,
+            },
+            body: JSON.stringify({
+                query: `
+                        query getProductDetail {
+                            site {
+                                product(sku: "${sku}") {
+                                    path
+                                    entityId
+                                }
+                            }
+                        }
+                    `,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (!data || !data.data?.site?.product) {
+                    return
+                }
+                const { product } = data.data.site
+                const path = product.path
+                if (path) {
+                    links.forEach((link) => {
+                        /* eslint-disable-next-line no-param-reassign */
+                        link.href = path
+                    })
+                }
+
+                const entityId = product.entityId
+                if (wishlistForm && entityId) {
+                    wishlistForm.action = `/wishlist.php?action=add&product_id=${entityId}`
+                }
+            })
+    })
+}
